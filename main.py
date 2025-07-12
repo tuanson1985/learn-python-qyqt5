459280f85149b05f941331740d086bbc9272267d
tuansonhq <105621622+tuanson1985@users.noreply.github.com>
2025-07-13 02:32:57 +0700

    update button

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sys
 import typing
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow, QLabel,
+    QWidget, QVBoxLayout, QHBoxLayout,
+    QGridLayout, QPushButton)
+from PyQt5.QtGui import QIcon, QFont, QPixmap
+from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -10,9 +14,87 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('HQGROUP')
         self.setGeometry(700,300,500,500)
         self.setWindowIcon(QIcon("logo.png"))
+        self.button = QPushButton("Click me!", self)
+        self.lable = QLabel("HQGROUP", self)
+        self.initUI() 
 
-        label = QLabel("Hello", self)
-        label.setFont(QFont("Arial", 30))
+    def initUI(self):
+        self.button.setGeometry(150, 200,200,100)
+        self.button.setStyleSheet("font-size: 30px")
+        self.button.clicked.connect(self.on_click)
+
+        self.lable.setGeometry(150,300,200,100)
+        self.lable.setStyleSheet("font-size: 50px")
+
+    def on_click(self):
+        self.lable.setText("Goodbuye!")
+        # print("Button click")
+        # self.button.setText("Clicked!")
+        # self.button.setDisabled(True)
+
+        # central_widget = QWidget()
+        # selt.setCentralWidget(central_widget)
+
+        # label1 = QLabel("#1",selt)
+        # label2 = QLabel("#2",selt)
+        # label3 = QLabel("#3",selt)
+        # label4 = QLabel("#4",selt)
+        # label5 = QLabel("#5",selt)
+
+        # label1.setStyleSheet("background-color: red")
+        # label2.setStyleSheet("background-color: yellow")
+        # label3.setStyleSheet("background-color: green")
+        # label4.setStyleSheet("background-color: blue")
+        # label5.setStyleSheet("background-color: purple")
+
+        # grid = QGridLayout()
+
+        # grid.addWidget(label1,0,0)
+        # grid.addWidget(label2,0,1)
+        # grid.addWidget(label3,1,0)
+        # grid.addWidget(label4,1,1)
+        # grid.addWidget(label5,2,2)
+
+        # central_widget.setLayout(grid)
+    
+        # hbox = QHBoxLayout()
+
+        # hbox.addWidget(label1)
+        # hbox.addWidget(label2)
+        # hbox.addWidget(label3)
+        # hbox.addWidget(label4)
+        # hbox.addWidget(label5)
+
+        # central_widget.setLayout(hbox)
+
+        # vbox = QVBoxLayout()
+
+        # vbox.addWidget(label1)
+        # vbox.addWidget(label2)
+        # vbox.addWidget(label3)
+        # vbox.addWidget(label4)
+        # vbox.addWidget(label5)
+
+        # central_widget.setLayout(vbox)
+
+        # label = QLabel("Hello", self)
+        # label.setFont(QFont("Arial", 30))
+        # label.setGeometry(0,0,500,100)
+        # label.setStyleSheet("color:blue")
+        # label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+        # label_two = QLabel(self)
+        # label_two.setGeometry(0,0,500,500)
+
+        # pixmap = QPixmap("background.jpg")
+        # label_two.setPixmap(pixmap)
+        # label_two.setScaledContents(True)
+        # label_two.setGeometry(
+        #     (self.width() - label_two.width()) // 2,
+        #     (self.height() - label_two.height()) // 2,
+        #     label_two.width(),
+        #     label_two.height()
+        #     )
+
 
 def main():
     app = QApplication(sys.argv)
