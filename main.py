4368b3c60443a514e6af3b22a6c7fa798b4d3e08
tuansonhq <105621622+tuanson1985@users.noreply.github.com>
2025-07-13 11:17:04 +0700

    update button radio

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PyQt5 import QtCore
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QLabel,
     QWidget, QVBoxLayout, QHBoxLayout, QCheckBox,
-    QGridLayout, QPushButton)
+    QGridLayout, QPushButton, QRadioButton, QButtonGroup)
 from PyQt5.QtGui import QIcon, QFont, QPixmap
 from PyQt5.QtCore import Qt
 
@@ -14,24 +14,61 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('HQGROUP')
         self.setGeometry(700,300,500,500)
         self.setWindowIcon(QIcon("logo.png"))
-        self.checkbox = QCheckBox("Do you like food?", self)
+        self.radio1 = QRadioButton("Visa", self)
+        self.radio2 = QRadioButton("Master card", self)
+        self.radio3 = QRadioButton("Gift card", self)
+        self.radio4 = QRadioButton("In-Store", self)
+        self.radio5 = QRadioButton("Online", self)
+
+        self.button_group1 = QButtonGroup(self)
+        self.button_group2 = QButtonGroup(self)
+
+        # self.checkbox = QCheckBox("Do you like food?", self)
         # self.button = QPushButton("Click me!", self)
         # self.lable = QLabel("HQGROUP", self)
         self.initUI() 
 
     def initUI(self):
-        self.checkbox.setGeometry(10,0,500,100)
-        self.checkbox.setStyleSheet("font-size: 16px;font-family: Arial")
-        self.checkbox.setChecked(False)
-        self.checkbox.stateChanged.connect(self.checkbox_changed)
-        
-    def checkbox_changed(self, state):
-        print(state)
+        self.radio1.setGeometry(10,0,500,100)
+        self.radio1.setStyleSheet("font-size: 16px;font-family: Arial")
+        self.radio2.setGeometry(10,50,500,100)
+        self.radio2.setStyleSheet("font-size: 16px;font-family: Arial")
+        self.radio3.setGeometry(10,100,500,100)
+        self.radio3.setStyleSheet("font-size: 16px;font-family: Arial")
+        self.radio4.setGeometry(10,150,500,100)
+        self.radio4.setStyleSheet("font-size: 16px;font-family: Arial")
+        self.radio5.setGeometry(10,200,500,100)
+        self.radio5.setStyleSheet("font-size: 16px;font-family: Arial")
+
+        self.button_group1.addButton(self.radio1)
+        self.button_group1.addButton(self.radio2)
+        self.button_group1.addButton(self.radio3)
+
+        self.button_group2.addButton(self.radio4)
+        self.button_group2.addButton(self.radio5)
+
+        self.radio1.toggled.connect(self.radio_button_changed)
+        self.radio2.toggled.connect(self.radio_button_changed)
+        self.radio3.toggled.connect(self.radio_button_changed)
+        self.radio5.toggled.connect(self.radio_button_changed)
+        self.radio4.toggled.connect(self.radio_button_changed)
+
+        # self.checkbox.setGeometry(10,0,500,100)
+        # self.checkbox.setStyleSheet("font-size: 16px;font-family: Arial")
+        # self.checkbox.setChecked(False)
+        # self.checkbox.stateChanged.connect(self.checkbox_changed)
+    def radio_button_changed(self):
+        radio_button = self.sender()
+        if radio_button.isChecked():
+            print(f"{radio_button.text()} is selected")
+    # def checkbox_changed(self, state):
+    #     print(state)
     
-        if state == Qt.Checked:
-            print("You like food")
-        else:
-            print("You do not like food")
+    #     if state == Qt.Checked:
+    #         print("You like food")
+    #     else:
+    #         print("You do not like food")
+
         # self.button.setGeometry(150, 200,200,100)
         # self.button.setStyleSheet("font-size: 30px")
         # self.button.clicked.connect(self.on_click)
